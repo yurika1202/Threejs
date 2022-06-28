9c1cc8e3185e879bfa466d58e29c164d3fa3e909
yurika1202 <yrpn120293@gmail.com>
2022-06-29 03:14:47 +0900

    stepup-06 クラスに書き換え #12

--- JavaScript/stepup/06/main.js
+++ JavaScript/stepup/06/main.js
@@ -1,67 +1,73 @@
+class AreaSelector {
+  constructor(root) {
+    this.rootElm = root;
+    this.prefectures = [];
+    this.cities = [];
+    this.prefCode = null;
+  }
+
+  // 初期化メソッド これを実行することでアプリが動く
+  async init() {
+    await this.updatePref();
+    await this.updateCity();
+  }
+
+  // AJAXで都道府県のJSONデータを取得し、オブジェクトとして返す
+  async getPrefs() {
+    const prefResponse = await fetch('./prefectures.json');
+    return await prefResponse.json();
+  }
+
+  // AJAXで市区町村のJSONデータを取得し、オブジェクトとして返す
+  async getCities(prefCode) {
+    const cityResponse = await fetch(`./cities/${prefCode}.json`);
+    return await cityResponse.json();
+  }
+
+  // 取得した都道府県JSONデータからoptionタグを生成しselectへ反映
+  async updatePref() {
+    this.prefectures = await this.getPrefs();
+    // 選択中の都道府県コード
+    this.prefCode = this.prefectures[0].code;
+    this.createPrefOptionsHtml();
+  }
+
+  // 取得した市区町村JSONデータの内容をselectへ反映
+  async updateCity() {
+    this.cities = await this.getCities(this.prefCode);
+    this.createCityOptionsHtml();
+  }
+
+  // HTMLの整備
+  createPrefOptionsHtml() {
+    const prefSelectorElm = this.rootElm.querySelector('.prefectures');
+    // join()：呼び出し元の配列を引数で与えた文字で区切りつつ連結（文字列化）する。
+    // 今回は引数が空なので区切り文字なしで連結。
+    prefSelectorElm.innerHTML = this.toOptionsHtml(this.prefectures);
+    // 都道府県選択時に市区町村の内容を変更する
+    prefSelectorElm.addEventListener('change', event => {
+      this.prefCode = event.target.value;
+      this.updateCity();
+    });
+  }
+  createCityOptionsHtml() {
+    const citySelectorElm = this.rootElm.querySelector('.cities');
+    citySelectorElm.innerHTML = this.toOptionsHtml(this.cities);
+  }
+
+  toOptionsHtml(records) {
+    return records
+      .map(record => {
+        return `
+          <option name="${record.name}" value="${record.code}">
+          ${record.name}
+          </option>
+        `;
+      })
+      .join('');
+  }
+}
+
 const rootElm = document.querySelector('.ariaSelector');
-
-// 初期化メソッド これを実行することでアプリが動く
-async function initAreaSelector() {
-  await updatePref();
-  await updateCity();
-}
-
-// AJAXで都道府県のJSONデータを取得し、オブジェクトとして返す
-async function getPrefs() {
-  const prefResponse = await fetch('./prefectures.json');
-  return await prefResponse.json();
-}
-
-// AJAXで市区町村のJSONデータを取得し、オブジェクトとして返す
-async function getCities(prefCode) {
-  const cityResponse = await fetch(`./cities/${prefCode}.json`);
-  return await cityResponse.json();
-}
-
-// 取得した都道府県JSONデータからoptionタグを生成しselectへ反映
-async function updatePref() {
-  const prefs = await getPrefs();
-  createPrefOptionsHtml(prefs);
-}
-
-// 取得した市区町村JSONデータの内容をselectへ反映
-async function updateCity() {
-  const prefSelectorElm = rootElm.querySelector('.prefectures');
-  const cities = await getCities(prefSelectorElm.value);
-  createCityOptionsHtml(cities);
-}
-
-// HTMLの整備
-function createPrefOptionsHtml(prefs) {
-  const optionStrs = [];
-  for (const pref of prefs) {
-    optionStrs.push(`
-      <option name="${pref.name}" value="${pref.code}">
-        ${pref.name}
-      </option>
-    `);
-  }
-  const prefSelectorElm = rootElm.querySelector('.prefectures');
-  // join()：呼び出し元の配列を引数で与えた文字で区切りつつ連結（文字列化）する。
-  // 今回は引数が空なので区切り文字なしで連結。
-  prefSelectorElm.innerHTML = optionStrs.join('');
-  // 都道府県選択時に市区町村の内容を変更する
-  prefSelectorElm.addEventListener('change', event => {
-    updateCity();
-  });
-}
-
-function createCityOptionsHtml(cities) {
-  const optionStrs = [];
-  for (const city of cities) {
-    optionStrs.push(`
-      <option name="${city.name}" value="${city.code}">
-        ${city.name}
-      </option>
-    `);
-  }
-  const citySelectorElm = rootElm.querySelector('.cities');
-  citySelectorElm.innerHTML = optionStrs.join('');
-}
-
-initAreaSelector();
+const areaSelector = new AreaSelector(rootElm);
+areaSelector.init();
