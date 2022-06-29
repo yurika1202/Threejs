d817aa8a73d9db121a0fa5dcb264d59ddf72402b
yurika1202 <yrpn120293@gmail.com>
2022-06-29 09:17:03 +0900

    anime-shuffleText #10

--- SandBox/anime/main.js
+++ SandBox/anime/main.js
@@ -1,17 +1,24 @@
 // スクロール量に応じて線が伸びる
 const ScrollLine = () => {
-  const line = document.querySelector(".line");
+  const line = document.querySelector('.line');
   const scrollTop = window.scrollY;
   const documentHeight = document.documentElement.scrollHeight;
   const viewHeight = document.documentElement.clientHeight;
   const scrollHeight = documentHeight - viewHeight;
   const scroll = (scrollTop / scrollHeight) * 100;
 
-  line.style.height = scroll + 10 + "%";
+  line.style.height = scroll + 10 + '%';
 
   if (scroll > 90) {
-    line.style.height = "100%";
+    line.style.height = '100%';
   }
 };
 
 window.onscroll = ScrollLine;
+
+// シャッフルテキスト（ライブラリ使用）
+const shuffleText = document.querySelectorAll('.shuffleText_lib');
+shuffleText.forEach(text => {
+  text = new ShuffleText(text);
+  text.start();
+});
